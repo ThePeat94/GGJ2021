c159b6ffb27c1ef4769f811e0f4477a8382c5afd
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2021-01-27 23:16:40 +0100

    Initial Commit

---
+++ Game/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_StartMenu;
+
+        public void BtnPlay_Click()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void BtnQuit_Click()
+        {
+            Application.Quit();
+        }
+    }
+}
